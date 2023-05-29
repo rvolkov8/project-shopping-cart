5e30d5cbcb8cf2ea633eef4f29c7997e470a0b1f
rvolkov8 <120594195+rvolkov8@users.noreply.github.com>
2023-05-29 19:21:53 +0300

    Add gh-pages for production

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import './styles/index.css';
 import './styles/header/Header.css';
@@ -21,7 +21,7 @@ import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
